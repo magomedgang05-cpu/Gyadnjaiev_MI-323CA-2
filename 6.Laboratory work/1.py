10742d9ff25bb735ea683b029a0477c184f857de
Gyandjaliev_MI <magomed.gang05@gmail.com>
2026-01-10 20:17:54 +0500

    Delete 6.Laboratory work directory

--- 6.Laboratory work/1.py
+++
@@ -1,9 +0,0 @@
-fio_input = input("Введите ваше ФИО через пробел: ")
-
-fio_parts = fio_input.split()
-
-formatted_parts = [part.capitalize() for part in fio_parts]
-
-formatted_fio = ' '.join(formatted_parts)
-
-print(f"Добро пожаловать, {formatted_fio}!")
--- 6.Laboratory work/3.py
+++
@@ -1,8 +0,0 @@
-text = input("Введите произвольный текст: ")
-
-step = int(input("Введите шаг: "))
-
-result = text[::step]
-
-print("\n Текст с шагом", step, ":")
-print(result)
